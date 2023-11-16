b5db87cef551f0b079f81920eb029794ec191b30
SuddenFlame414 <74963748+SuddenFlame414@users.noreply.github.com>
2023-11-16 15:59:56 +0530

    Add files via upload

---
+++ Project 43/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A E", 500,300)
+    fill("lightblue")
+    text("Hint: Place to monitor parts in code LIVE !!", 500,320)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Set of code used with one WORD !!", 700,170)
+
+}
